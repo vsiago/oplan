75f7d623b61e67aeb8a5d9c80c5becf348882097
vsiago <vsiago23@gmail.com>
2023-02-08 08:10:30 -0300

    vai

--- src/js/TopMenuServices.js
+++ src/js/TopMenuServices.js
@@ -1,10 +1,9 @@
 window.addEventListener("scroll", function(){
-    if (window.scrollY >= this.window.screenY * this.window.screenY) {
+    if (window.scrollY > this.window.screenY * this.window.screenY) {
     const servicesMenu = document.querySelector('#servicesMenu')
     servicesMenu.style.position = 'fixed';
     servicesMenu.style.top = '0';
     servicesMenu.style.width = '100%';
-
 
     } else {
         servicesMenu.style.position = 'static'
