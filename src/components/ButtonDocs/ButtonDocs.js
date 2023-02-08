39fba493e8277038839c7c9190bb81d78637cc8f
Iago Souza Vieira <iago.vieira@itg.rio>
2023-02-08 16:29:31 -0300

    atualizações gerais

--- src/components/ButtonDocs/ButtonDocs.js
+++ src/components/ButtonDocs/ButtonDocs.js
@@ -1,6 +1,20 @@
 const btnDocs = document.querySelector('#btnDocs');
-const listaMenu = document.querySelector('#listaMenu')
+const listaMenu = document.querySelector('#listaMenu');
 
-btnDocs.addEventListener("click", function() {
-    listaMenu.classList.toggle('hidden')
+
+btnDocs.addEventListener("mouseover", function() {
+    listaMenu.style.display = "block";
+})
+
+listaMenu.addEventListener("mouseout", function () {
+    listaMenu.style.display = "none";
+});
+
+
+const subMenu = document.querySelector('#animar-sub-menu')
+const listaSubMenu = document.querySelector('#listaSubMenu')
+
+
+subMenu.addEventListener("click", function () {
+    listaSubMenu.classList.toggle('hidden');
 })
