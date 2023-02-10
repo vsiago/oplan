68b6c315f1f9f02eaa41bb02777e3108ff693e7d
vsiago <vsiago22@gmail.com>
2023-02-10 18:34:06 -0300

    atualizações gerais

--- src/components/ButtonDocs/ButtonDocs.js
+++ src/components/ButtonDocs/ButtonDocs.js
@@ -1,35 +1,35 @@
 // Animar Menu Hover
-const btnDocs = document.querySelector('#btnDocs');
-const listaMenuHover = document.querySelector('#listaMenuHover');
-const arrowBtn = document.querySelector('#arrowBtn');
+const btnDocs = document.querySelector("#btnDocs");
+const listaMenuHover = document.querySelector("#listaMenuHover");
+const arrowBtn = document.querySelector("#arrowBtn");
 
-
-btnDocs.addEventListener("mouseover", function() {
-    listaMenuHover.style.display = "block";
-    listaMenuHover.classList.add('w-[90%]')
-})
-
-
-
-listaMenuHover.addEventListener("mouseout", function () {
-    listaMenuHover.style.display = "none";
+btnDocs.addEventListener("mouseover", function () {
+  listaMenuHover.style.display = "block";
+  listaMenuHover.classList.add("w-[90%]");
 });
 
+listaMenuHover.addEventListener("mouseout", function () {
+  listaMenuHover.style.display = "none";
+});
+
+btnDocs.addEventListener("click", function () {
+  listaMenuHover.classList.toggle("hidden");
+});
 
 // SubMenu Regulamentações ANS
 
-const btnRegAns = document.querySelector('.btnRegAns')
-const regAns = document.querySelector('#regAns')
+const btnRegAns = document.querySelector(".btnRegAns");
+const regAns = document.querySelector("#regAns");
 
 btnRegAns.addEventListener("click", function () {
-    regAns.classList.toggle('hidden');
-})
+  regAns.classList.toggle("hidden");
+});
 
 // SubMenu Qualificações de Operadoras qualiOpe
 
-const btnQualiOpe = document.querySelector('.btnQualiOpe')
-const qualiOpe = document.querySelector('#qualiOpe')
+const btnQualiOpe = document.querySelector(".btnQualiOpe");
+const qualiOpe = document.querySelector("#qualiOpe");
 
 btnQualiOpe.addEventListener("click", function () {
-    qualiOpe.classList.toggle('hidden');
-})
+  qualiOpe.classList.toggle("hidden");
+});
