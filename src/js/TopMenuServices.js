3a1cda77e69b6daf3ba3de974fe6a0ec144e4eba
vsiago <vsiago22@gmail.com>
2023-02-12 18:21:21 -0300

    novo menu hamburguer apenas com css

--- src/js/TopMenuServices.js
+++ src/js/TopMenuServices.js
@@ -1,11 +1,10 @@
-window.addEventListener("scroll", function(){
-    if (window.scrollY > window.screen.availHeight - 100 + 451) {
-    const servicesMenu = document.querySelector('#servicesMenu')
-    servicesMenu.style.position = 'fixed';
-    servicesMenu.style.top = '0';
-    servicesMenu.style.width = '100%';
-
-    } else {
-        servicesMenu.style.position = 'static'
-    }
-})
+window.addEventListener("scroll", function () {
+  if (window.scrollY > window.screen.availHeight - 100 + 451) {
+    const servicesMenu = document.querySelector("#servicesMenu");
+    servicesMenu.style.position = "fixed";
+    servicesMenu.style.top = "0";
+    servicesMenu.style.width = "100%";
+  } else {
+    servicesMenu.style.position = "static";
+  }
+});
