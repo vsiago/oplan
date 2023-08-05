1ec6e64ce473c16b120f51681e995d2158cf1f9c
vsiago <vsiago22@gmail.com>
2023-08-05 09:16:21 -0300

    validações basicas dos formularios ok

--- src/components/Hero/SalveLeads.js
+++ src/components/Hero/SalveLeads.js
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,49 +14,54 @@ const firebaseConfig = {
   projectId: "oplan-4e3e6",
   storageBucket: "oplan-4e3e6.appspot.com",
   messagingSenderId: "1062637390846",
-  appId: "1:1062637390846:web:65928175677f800e588eaf"
+  appId: "1:1062637390846:web:65928175677f800e588eaf",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-// PC
-const inputNamePc = document.querySelector('.inputNamePc');
-const inputTelPc = document.querySelector('.inputTelPc');
-const btnReceberPropostaPc = document.querySelector('.btnReceberPropostaPc');
+// PC /////////////////////////////////////////////
+const inputNamePc = document.querySelector(".inputNamePc");
+const inputTelPc = document.querySelector(".inputTelPc");
+const formPc = document.querySelector(".formPc");
 
-btnReceberPropostaPc.addEventListener('click', (event) => {
+function enviarFormularioPc(event) {
   event.preventDefault();
 
   const name = inputNamePc.value;
   const whatsApp = inputTelPc.value;
 
   create(name, whatsApp);
-});
+}
 
+formPc.addEventListener("submit", enviarFormularioPc);
 
-// MOBILE
-const inputNameMob = document.querySelector('.inputNameMob');
-const inputTelMob = document.querySelector('.inputTelMob');
-const btnReceberPropostaMob = document.querySelector('.btnReceberPropostaMob');
+// MOBILE /////////////////////////////////////////////
+const inputNameMob = document.querySelector(".inputNameMob");
+const inputTelMob = document.querySelector(".inputTelMob");
+const formMobile = document.querySelector(".formMobile");
 
-btnReceberPropostaMob.addEventListener('click', (event) => {
+function enviarFormularioMobile(event) {
   event.preventDefault();
 
   const name = inputNameMob.value;
   const whatsApp = inputTelMob.value;
 
+
+
   create(name, whatsApp);
-});
+}
+
+formMobile.addEventListener("submit", enviarFormularioMobile);
 
 function create(name, whatsApp) {
-  const leadsRef = ref(database, 'leads');
+  const leadsRef = ref(database, "leads");
   const newLeadRef = push(leadsRef);
 
   const data = {
     name: name,
-    whatsApp: whatsApp
+    whatsApp: whatsApp,
   };
 
   set(newLeadRef, data)
